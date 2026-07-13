c9101a8e423feefbf15522084974a79531b3b6a8
archit_1512 <architchaudhary.bt24cseai@pec.edu.in>
2026-07-13 17:34:10 +0000

    conversions

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -1,9 +1,39 @@
 "use strict";
-let name="archit";  
-let age=21;
-let varify="true";
+
+let name = "archit";
+let age = 21;
+let verify = true;
+
+console.table({
+  name,
+  age,
+  verify
+});
+
+console.log(typeof "21"); // string
+console.log(typeof 21);   // number
+console.log(typeof verify); // boolean
+console.log(typeof null);
+console.log(typeof undefined);
 
 
+let newage=33
+console.log(typeof age);
 
-console.table(name,age, varify);
-console.log(typeof 21);
+let newage2="22abc"
+console.log(typeof newage2) // type of mei agr newage ki value null hoti then ans 0, agr value undefined hoti toh NaN not a number
+console.log(newage2)
+
+
+let checkconversion=Number(newage2)
+console.log(typeof checkconversion)
+console.log(checkconversion)
+
+
+let newnum=33
+console.log(newnum)
+console.log(typeof newnum)
+
+let changednn=String(newnum)
+console.log(changednn)
+console.log(typeof changednn)
